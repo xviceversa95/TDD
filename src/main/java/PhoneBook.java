3897dad117f8aed1603dcc613332c909ffc6777f
viceversa95 <meshalnikova@gmail.com>
2025-07-05 18:50:51 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
 
+    // тут будем хранить имена и номера:
+    public Map<String, Integer> phoneBook = new HashMap<>();
+
+
+    public static int add(String name, Integer number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    public void testAddContact() {
+        PhoneBook testBook = new PhoneBook();
+        int expected = 1;
+        assertEquals(expected, testBook.add("Anna", 555555));
+    }
 }
