2b2c08c6f2ed18418c5d270462c128e1b9d52928
viceversa95 <meshalnikova@gmail.com>
2025-07-16 12:57:15 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -4,10 +4,11 @@ import java.util.Map;
 public class PhoneBook {
 
     // тут будем хранить имена и номера:
-    public Map<String, Integer> phoneBook = new HashMap<>();
+    public static Map<String, Integer> phoneBook = new HashMap<>();
 
 
     public static int add(String name, Integer number) {
-        return 0;
+        phoneBook.put(name, number);
+        return phoneBook.size();
     }
 }
